ca248d453ba17487178dafae7a360777414ccddc
tfageo18 <thomas.fageol@gmail.com>
2013-12-30 18:11:54 +0100

    A tester
    Tester installation et faire des imprimes écrans.

--- install.bat
+++ install.bat
@@ -1,7 +1,10 @@
 @echo off
 cls
-
+rem *******************************
+rem 		Les VARIABLES
+rem *******************************
 SET currentpath=%~dp0
+SET heure=
 
 echo ****************************************************************
 echo **                                                            **
@@ -10,10 +13,9 @@ echo **                                                            **
 echo ****************************************************************
 
 c:
-cd "%currentpath%exe"
-BleachBit-1.0-setup.exe
-CleanUp452.exe
-ccsetup409.exe /S
+"%currentpath%exe\BleachBit-1.0-setup.exe"
+"%currentpath%exe\CleanUp452.exe"
+"%currentpath%exe\ccsetup409.exe"
 
 
 echo ****************************************************************
@@ -21,10 +23,10 @@ echo **                                                            **
 echo **              PARAMETRAGE DES SOFTS TIERS	               **
 echo **                                                            **
 echo ****************************************************************
-C:\Program Files (x86)\BleachBit\bleachbit.exe
-C:\Program Files (x86)\CleanUp!\cleanup.exe
-C:\Program Files\CCleaner\ccleaner.exe
-cleanmgr/sageset:1
+"C:\Program Files (x86)\BleachBit\bleachbit.exe"
+"C:\Program Files (x86)\CleanUp!\cleanup.exe"
+"C:\Program Files\CCleaner\ccleaner.exe"
+cleanmgr /sageset:1
 
 
 echo ****************************************************************
@@ -32,7 +34,7 @@ echo **                                                            **
 echo **              PARAMETRAGE DE LA TACHE PLANIFIE              **
 echo **                                                            **
 echo ****************************************************************
-SET heure=
+
 SET /p heure=Saisir l'heure de la tache (format hh:mm):
 rem "if you are on an english serve use this command
 rem at %heure% /EVERY: Monday,Tuesday,Wednesday,Thursday,Friday "%currentpath%axioclean.bat"
